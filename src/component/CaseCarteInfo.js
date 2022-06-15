f51bfd9607a7b551dfd3afcde0d69def9ebcdaef
ZephirSD <87165268+ZephirSD@users.noreply.github.com>
2022-06-15 19:35:53 +0400

    Création du composant `CaseCarteInfo`

---
+++ src/component/CaseCarteInfo.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Carte from './Carte'
+
+function CaseCarteInfo() {
+  return (
+    <>
+      <div className='grid-case-info-carte'>     
+        <div></div> 
+        <div className='div-carte'>
+            <Carte/>
+        </div>
+      </div>    
+    </>
+  )
+}
+
+export default CaseCarteInfo
