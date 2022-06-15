28f55f464ab27cf3525ff3792c61828bbdc59fd4
ZephirSD <87165268+ZephirSD@users.noreply.github.com>
2022-06-15 19:33:50 +0400

    Insertion des icones & modification de conditions pour le changements de couleurs

--- src/component/Indicateur.js
+++ src/component/Indicateur.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
 
-function Indicateur({title, num_base, total}) {
+function Indicateur({title, num_base, total, colorIndic}) {
   return (
     <>
         <div className='case-indic'>
             <div className='title-indic'>
                 {title}
             </div>
-            <div className='numbase-indic'>
+            <div className='numbase-indic' style={num_base > 0 ? {color: colorIndic} : {color: 'white'}}>
+                <FontAwesomeIcon icon={faArrowUp} className="icon-up" style={num_base > 0 ? {color: colorIndic, visibility:'visible'} : {visibility: 'hidden'}}/>
                 {num_base}
             </div>
             <div className='total-indic' style={title === 'Tests effectués' ? {color: 'white'} : {color: '#253140'}}>
