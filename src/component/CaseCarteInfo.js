e5a3cce46c20ac4d56eaa5d2e611075a639d3867
ZephirSD <87165268+ZephirSD@users.noreply.github.com>
2022-06-16 09:36:54 +0400

    Insertion du composant `Actualite`

--- src/component/CaseCarteInfo.js
+++ src/component/CaseCarteInfo.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import Actualite from './Actualite'
 import Carte from './Carte'
 
 function CaseCarteInfo() {
   return (
     <>
-      <div className='grid-case-info-carte'>     
-        <div></div> 
+      <div className='grid-case-info-carte'>
+        <Actualite/>
         <div className='div-carte'>
             <Carte/>
         </div>
