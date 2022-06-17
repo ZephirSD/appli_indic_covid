9ceb94a521a4d2e09a2bb5d168859e7fc6c94970
ZephirSD <87165268+ZephirSD@users.noreply.github.com>
2022-06-17 13:50:22 +0400

    Intégration des clusters pour la Carte

--- src/component/Carte.js
+++ src/component/Carte.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
-import Map from "react-map-gl";
+import Map , {NavigationControl, Marker} from "react-map-gl";
+import { clusters } from './assets/cluster';
 function Carte() {
   const [viewport, setViewport] = useState({
     latitude: -21.131,
@@ -9,11 +10,18 @@ function Carte() {
   return (
     <>
       <Map
-       {...viewport}
-       mapStyle={"mapbox://styles/zephirsd18/cl1rdsvcb003y14qsjk3u0kc9"}
-       mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-       onViewportChange={setViewport}
-      />
+        initialViewState={viewport}
+        mapStyle={"mapbox://styles/zephirsd18/cl1rdsvcb003y14qsjk3u0kc9"}
+        mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        onViewportChange={setViewport}
+      >
+        <NavigationControl style={{marginRight: '15px', marginTop: '15px'}}/>
+        {clusters.map((cluster) => (
+          <Marker longitude={cluster.longitude} latitude={cluster.latitude} key={cluster.id}>
+              <div className='circle-cluster' style={{width: 'calc(25px*' + cluster.nombre * 0.7 + ')', height: 'calc(25px*' + cluster.nombre * 0.7 + ')'}}>{cluster.nombre}</div>
+          </Marker>
+        ))}
+      </Map>
     </>
   )
 }
