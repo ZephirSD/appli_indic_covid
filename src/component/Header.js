c228355f5c48e5cfac9a86982ab56ca431ce1e8b
ZephirSD <87165268+ZephirSD@users.noreply.github.com>
2022-06-19 13:55:31 +0400

    Création du responsive avec un système de grille en version ordinateur et un sysème de Swipe en version mobile

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,8 +1,16 @@
 import React, { useEffect, useState } from 'react'
-import Indicateur from './Indicateur'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper';
+import 'swiper/css';
+import "swiper/css/pagination";
+import Indicateur from './Indicateur';
 
 function Header() {
     const [data, setData] = useState([]);
+    const screenMobile = "(max-width: 720px)";
+    const [queryMobile, setQueryMobile] = useState({
+        matches: window.innerWidth < 720 ? true : false,
+    })
     const diseaseAPI = () => {
         fetch('https://disease.sh/v3/covid-19/countries/R%C3%A9union?yesterday=true&twoDaysAgo=true&strict=true&allowNull=0')
         .then((response) => response.json())
@@ -13,14 +21,43 @@ function Header() {
     return (
         useEffect(()=>{
             diseaseAPI();
+            let screenQuery = window.matchMedia(screenMobile);
+            screenQuery.addEventListener('change',setQueryMobile);
         },[]),
         <>
+        {
+            queryMobile && !queryMobile.matches ? (
             <div className='header'>
                 <Indicateur title={'Cas'} num_base={data.todayCases} total={data.cases} colorIndic={'#D97777'}/>
                 <Indicateur title={'Guérisons'} num_base={data.todayRecovered} total={data.recovered} colorIndic={'#9ED599'}/>
                 <Indicateur title={'Décès'} num_base={data.todayDeaths} total={data.deaths} colorIndic={'#D97777'}/>
                 <Indicateur title={'Tests effectués'} total={data.tests}/>
             </div>
+            ) : 
+            (
+                <Swiper
+                    spaceBetween={20}
+                    modules={[Pagination]}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    className="swiperHeader"
+                >
+                    <SwiperSlide>
+                        <Indicateur title={'Cas'} num_base={data.todayCases} total={data.cases} colorIndic={'#D97777'}/>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <Indicateur title={'Guérisons'} num_base={data.todayRecovered} total={data.recovered} colorIndic={'#9ED599'}/>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <Indicateur title={'Décès'} num_base={data.todayDeaths} total={data.deaths} colorIndic={'#D97777'}/>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <Indicateur title={'Tests effectués'} total={data.tests}/>
+                    </SwiperSlide>
+                </Swiper>
+            )
+        }
         </>
     )
 }
