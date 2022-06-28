77845dc7e00616595b6d77f0705e5c90ee1cfd2c
ZephirSD <87165268+ZephirSD@users.noreply.github.com>
2022-06-28 15:21:46 +0400

    Modification des nombres indicateurs en `toLocateString()`

--- src/component/Indicateur.js
+++ src/component/Indicateur.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
 
 function Indicateur({title, num_base, total, colorIndic}) {
+    const [numBaseState, setNumBaseState] = useState();
+    const [totalState, setTotalState] = useState();
+    const requestNumBaseProps = async () => {
+        const resultNumBase = await num_base;
+        if(resultNumBase !== undefined){
+            let strNumBase = resultNumBase.toLocaleString();
+            setNumBaseState(strNumBase);
+        }
+
+    }
+    const requestTotalProps = async () => {
+        const resultTotal = await total;
+        if(resultTotal !== undefined){
+            let strTotal = resultTotal.toLocaleString();
+            setTotalState(strTotal);
+        }
+    }
+    useEffect(() => {
+        requestNumBaseProps();
+        requestTotalProps();
+    })
   return (
     <>
         <div className='case-indic'>
@@ -11,10 +32,10 @@ function Indicateur({title, num_base, total, colorIndic}) {
             </div>
             <div className='numbase-indic' style={num_base > 0 ? {color: colorIndic} : {color: 'white'}}>
                 <FontAwesomeIcon icon={faArrowUp} className="icon-up" style={num_base > 0 ? {color: colorIndic, visibility:'visible'} : {visibility: 'hidden'}}/>
-                {num_base}
+                {numBaseState}
             </div>
             <div className='total-indic' style={title === 'Tests effectués' ? {color: 'white'} : {color: '#253140'}}>
-                {total}
+                {totalState}
             </div>
         </div>
     </>
